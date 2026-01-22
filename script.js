4691179451989c47ccb140285f48b2b579fa419a
Giosho <114431239+Giosho@users.noreply.github.com>
2026-01-22 23:00:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-// JS за случайно място
-const places = [
-    "Sticky Fingers music club",
-    "Gothenburg Concert Hall",
-    "Local underground metal venues"
-];
+<script>
+function openCity(evt, cityName){
+    var i, tabcontent = document.getElementsByClassName("tabcontent");
+    for(i=0;i<tabcontent.length;i++) tabcontent[i].style.display="none";
+    document.getElementById(cityName).style.display="block";
 
-function showPlace() {
-    const index = Math.floor(Math.random() * places.length);
-    document.getElementById('place').textContent = places[index];
+    // Смяна на фон с плавно fade
+    if(cityName=="Oslo"){
+        document.body.style.backgroundImage = "url('norway-flag.jpg')";
+    } else if(cityName=="Stockholm"){
+        document.body.style.backgroundImage = "url('sweden-flag.jpg')";
+    }
 }
+
+function openSubTab(evt, subTabId){
+    var i, subtabs = evt.currentTarget.parentNode.parentNode.getElementsByClassName("subtab");
+    for(i=0;i<subtabs.length;i++) subtabs[i].style.display="none";
+    document.getElementById(subTabId).style.display="block";
+}
+
+window.onload = function() {
+    document.getElementById("defaultOpen").click();
+}
+</script>
